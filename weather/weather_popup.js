2ae5ea6ecd649388238f700b3a83a4ac4d060187
mitrotasios <a.mitrotasios13@gmail.com>
2021-01-08 16:41:11 +0000

    Callback version of geolocation

--- weather/weather_popup.js
+++ weather/weather_popup.js
@@ -5,12 +5,52 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 */
 
+/*
 document.addEventListener('DOMContentLoaded', function () {
 
-  //var test = document.getElementById("firstText").innerHTML;
-  var cityName = "LOS ANGELES"
-  var countryName = "USA"
-  document.getElementById("cityName").innerHTML=cityName;
-  document.getElementById("countryName").innerHTML=countryName;
- 
+  chrome.extension.getBackgroundPage().getLocation();
+
+  
+  var cityName = "LOS ANGELES";
+  var countryName = "USA";
+  document.getElementById("cityName").innerHTML=lat;
+  document.getElementById("countryName").innerHTML=lng;
 });
+*/
+
+function test() {
+  
+  function getUserLocation(callback) {
+
+    function success(position) {
+      
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      callback(latitude);
+    }
+
+    function error() {
+      var locationError = 'Unable to retrieve your location';
+      console.log(locationError);
+    }
+
+    if(!navigator.geolocation) {
+      var unsupportedError = 'Geolocation is not supported by your browser';
+      console.log(unsupportedError);
+    } else {
+      //status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+  }
+
+  getUserLocation (function(latitude){
+    alert(latitude);
+    document.getElementById("cityName").innerHTML=latitude;
+    //document.getElementById("countryName").innerHTML=longitude;
+  })
+
+}
+
+document.addEventListener('DOMContentLoaded', test);
--- weather_bg.js
+++ weather_bg.js
@@ -3,21 +3,27 @@
 function backgroundFunction() {
     alert('Background, reporting for duty!')
 }
-*/
 
-/*
+
 function getLocation() {
+    console.log("Background running")
     if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(getVals) 
     } else { 
     //x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition(position) {
-    window.location = position.coords.latitude;
+function getVals(position) {
+    
+    location = {
+        window.lat: position.coords.latitude,
+        window.lng: position.coords.longitude    
+    }
+
     
     //x.innerHTML = "Latitude: " + position.coords.latitude + 
     //"<br>Longitude: " + position.coords.longitude;
 }
+
 */
