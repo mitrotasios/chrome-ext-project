cbae25bdccbc71f27a0db0697881265e4b9f9b84
mitrotasios <a.mitrotasios13@gmail.com>
2021-02-23 23:48:13 +0200

    comments

--- weather/popup.js
+++ weather/popup.js
@@ -1,7 +1,11 @@
 
 function mapWeatherIcons(currentDesc) {
-    var l_img_url;
-    var sm_img_url;
+    // Function which maps the associated icon to a given weather condition
+    // using a switch statement
+    // return: array containing a small and a large weather icon path
+
+    var l_img_url; // large icon
+    var sm_img_url; // small uicon
     switch(currentDesc) {
         case "Thunderstorm":
         l_img_url = "../imgs/thunderstorm-grey.png";
@@ -78,47 +82,50 @@ function mapWeatherIcons(currentDesc) {
 }
 
 function setBackground(cityName) {
-    var cityName = cityName.replace(/ /g," ");
-    console.log("CityName", cityName);
+    // Function fetching the app background depending on the city, using the Pexels API.
+    
+    var cityName = cityName.replace(/-/g," ");
 
-    const KEY = '563492ad6f9170000100000172dbe915a2bc4ce294d3e5fb719fdc14';
+    const KEY = '563492ad6f9170000100000172dbe915a2bc4ce294d3e5fb719fdc14'; // API Key
 
+    // fetch API
     fetch(`https://api.pexels.com/v1/search?query=${cityName}&per_page=10`, {
         headers: {
         Authorization: KEY
         }
     })
-    .then(response => response.json())  // convert to json
-    .then(json => insertImage(json))    //print data to console
-    .catch(err => console.log('Request Failed', err)); // Catch errors
+    .then(response => response.json()) 
+    .then(json => insertImage(json)) // callback function if promise is successfull
+    .catch(err => console.log('Request Failed', err)); // Catch error
 }
 
 function insertImage(json) {
-    console.log("Pexels", json);
-    console.log("Done");
+    // callback function from fetch API call
 
+    // picks a ranfom number from 0-9 and picks image from response
     let i = Math.floor(Math.random() * 10);
     bg_img_url = json.photos[i].src.large;
 
+    // replaces background in html script
     document.getElementById("bg-img").src=bg_img_url;
 }
 
 function fetchLocation(latitude, longitude) {
-    console.log("fetchLocationLatLong", latitude, longitude)
+    // Fetches Google Reverse Geocoding API to retrieve (largest) city name
     const KEY = 'AIzaSyCl8xuKwal-sSnAXAvGzjlhfG--HT_m57Q';
     const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${KEY}`;
 
-    // GET Request.
+    // Fetch API GET Request
     fetch(url)
-    // Handle success
+    // Handle promise
     .then(response => response.json())  // convert to json
-    .then(json => insertLocationName(json))    //print data to console
-    .catch(err => console.log('Request Failed', err)); // Catch errors
+    .then(json => insertLocationName(json))    // call insertLocationName function if successfull promise
+    .catch(err => console.log('Request Failed', err)); // catch errors
 }
 
 function insertLocationName(json) {
-    console.log("Reverse Geocoding", json);
-    console.log("cityName before loop", cityName);
+    // Callback function from API request
+    // retrieve city and country name from response / json 
     var part = json.results[3].address_components;
     console.log("Loop starts");
     for (var i = 0; i<part.length; i++) {
@@ -130,28 +137,30 @@ function insertLocationName(json) {
             var countryName = current.long_name;
         }
     }
-    console.log("cityName", cityName);
-    console.log("countryName", countryName);
+
+    // Change app title in html script
     document.getElementById("cityName").innerHTML=cityName.toUpperCase();
     document.getElementById("countryName").innerHTML=countryName.toUpperCase();
 
+    // call setBackground function
     setBackground(cityName);  
 }
 
 function fetchWeather(lat, lon) {
+    // Fetch weather data from openWeather API
     const KEY = 'bea7fbb8ded1b3f8c08fd623c25069cb';
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${KEY}&units=metric`;
 
-    // GET Request.
+    // GET Request
     fetch(url)
     // Handle success
     .then(response => response.json())  // convert to json
-    .then(json => insertWeather(json))    //print data to console
+    .then(json => insertWeather(json))    // call insertWeather if promise successfull
     .catch(err => console.log('Request Failed', err)); // Catch errors
 }
 
 function insertCurrent(json) {
-    console.log("Weather", json);
+    // Inserting current weather data into html script
     var currentTemp = String(Math.round(json.current.temp));
     var currentDesc = String(json.current.weather[0].main);
     var currentWind = String(json.current.wind_speed);
@@ -173,7 +182,9 @@ function insertCurrent(json) {
 }
 
 function insertForecast(json) {
-    var d = new Date();
+    // Creates weather forcast (5 days)
+    
+    // Map forecast weekday names
     var weekday = new Array(7);
     weekday[0] = "SUN";
     weekday[1] = "MON";
@@ -205,52 +216,53 @@ function insertForecast(json) {
 }
 
 function insertWeather(json) {
-    insertCurrent(json);
-    insertForecast(json);
+  // inserts current weather data  
+  insertCurrent(json);
+
+  // insearts forcast data
+  insertForecast(json);
 }
 
-function getWeatherCurrentLocation() {  
-  function success(position) {
-    console.log("success");
-    console.log("LS",localStorage.lat);
-    console.log("LS",localStorage.lon);
-    var latitude;
-    var longitude;
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+//// Event listener callback function
+function getWeatherCurrentLocation() {    
 
-    console.log("lat", latitude);
-    console.log("long", longitude);
+  function success(position) {
+    // Callback function from getCurrentPosition method receiving position argument
+    // returns latitude and longitude
+    
+    var latitude = position.coords.latitude; // get lat
+    var longitude = position.coords.longitude; // get long
+
+    // call fetchLocation function, in order to get the city & country name
     fetchLocation(latitude, longitude);
+
+    // call fetchWeather function, in order to get the weather data for the specific lat & lon
     fetchWeather(latitude, longitude);
   }
 
   function error() {
-    console.log("error");
+    // Error callback function
     var locationError = 'Unable to retrieve your location';
     console.log(locationError);
   }
 
-  console.log("running");
+  // if geolocation supported call getCurrentPosition method
+  // else return error
   if(!navigator.geolocation) {
     var unsupportedError = 'Geolocation is not supported by your browser';
     console.log(unsupportedError);
   } else {
-    console.log("get lat long");
-    navigator.geolocation.getCurrentPosition(success, error);
+    // getCurrentPosition method call
+    navigator.geolocation.getCurrentPosition(success, error); // success, error callback functions
   }
 
 }
 
+// Handle autocomplete using Google Maps autocomplete
 let autocomplete;
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'),
-        {
-            //types: [],
-            //componentRestrictions: {'country': ['AU']},
-            //fields: ['place_id', 'geometry', 'name']
-        });
+        document.getElementById('autocomplete'),{});
 
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
@@ -263,11 +275,13 @@ function onPlaceChanged() {
   } else {
     var latitude = String(place.geometry.location.lat());
     var longitude = String(place.geometry.location.lng());
-    console.log("new latlong", typeof latitude);
+    
+    // When new place selected trigger fetching process
     fetchLocation(latitude, longitude);
     fetchWeather(latitude, longitude);
   }
 
 }
 
+// when DOM content loaded get weather of user's current location
 document.addEventListener('DOMContentLoaded', getWeatherCurrentLocation);
